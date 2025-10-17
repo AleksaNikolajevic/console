72ddeaf3454385f95ad60af59a4a306f0060a3f5
AleksaNikolajevic <75322640+AleksaNikolajevic@users.noreply.github.com>
2025-10-17 16:57:49 +0200

    a

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System.IO.Pipes;
+
+class Program
+{
+    static void Main()
+    {
+        using (NamedPipeClientStream client = new NamedPipeClientStream(".", "MyWpfAppPipe", PipeDirection.Out))
+        {
+            client.Connect();
+
+            using (StreamWriter writer = new StreamWriter(client))
+            {
+                writer.AutoFlush = true;
+                writer.WriteLine("OpenPage2"); // Or OpenPage1
+            }
+        }
+
+        Console.WriteLine("Command sent.");
+    }
+}
