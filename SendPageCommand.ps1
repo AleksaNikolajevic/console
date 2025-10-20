c246e42a7d685d4a2343d4ba39fb1f906adb301e
AleksaNikolajevic <75322640+AleksaNikolajevic@users.noreply.github.com>
2025-10-20 20:15:16 +0200

    update

---
+++ SendPageCommand.ps1
@@ -0,0 +1,16 @@
+$pipeName = "MyWpfAppPipe"
+
+# Connect to the named pipe (client side)
+$pipeClient = new-object System.IO.Pipes.NamedPipeClientStream(".", $pipeName, [System.IO.Pipes.PipeDirection]::Out)
+$pipeClient.Connect()
+
+# Create a StreamWriter to send the command
+$writer = new-object System.IO.StreamWriter($pipeClient)
+$writer.AutoFlush = $true
+
+# Send the command to the WPF app
+$writer.WriteLine("OpenPage1")   # Or "OpenPage2" depending on what you want
+
+# Close resources
+$writer.Close()
+$pipeClient.Close()
